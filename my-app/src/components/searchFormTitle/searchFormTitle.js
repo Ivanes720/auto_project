1a9c72e1fb4290b03b746899d81c469268ae52a8
ivan <your_email@whatever.com>
2021-08-02 21:31:53 +0200

    api_connect_first

--- my-app/src/components/searchFormTitle/searchFormTitle.js
+++ my-app/src/components/searchFormTitle/searchFormTitle.js
@@ -10,19 +10,24 @@ const searchFormTitle = () => {
     );
     if (res.ok) {
       const data = await res.json();
-      console.log(data);
-
-      return data.forEach((item) => {
-        const selectElem = document.getElementById("categoriesss");
-        const options = createNode("options");
-        options.value = item.name;
-        selectElem.appendChild(options);
-      });
+      
+      return data.forEach(elem => {
+        for (let elem = 0; elem < data.length; elem++) 
+        {
+          const selectElem = document.getElementById("categories");
+          const option = createNode("option");
+          option.textContent = data[elem].name;
+          selectElem.appendChild(option);
+        }
+      });;
     } else {
       console.log("error, res.status");
     }
+
   };
+
   getAllData();
+
   return (
     <div className="form_search">
       <form>
@@ -34,72 +39,45 @@ const searchFormTitle = () => {
         </div>
         <div className="wrapper">
           <div className="nav">
-            <input
-              id="allRadioType"
-              type="radio"
-              value="all"
-              checked="checked"
-            />
-            <label for="allRadioType">
+            <input id="allRadioType" type="radio" />
+            <label>
               <span>Все</span>{" "}
             </label>
 
-            <input
-              id="buRadioType"
-              type="radio"
-              value="bu"
-            />
-            <label for="buRadioType">
+            <input id="buRadioType" type="radio" />
+            <label>
               <span>Б/у</span>{" "}
             </label>
 
-            <input
-              id="naRadioType"
-              type="radio"
-              value="na"
-            />
-            <label for="naRadioType">
+            <input id="naRadioType" type="radio" />
+            <label>
               <span>Новые авто</span>
             </label>
-            <input
-              id="orderRadioType"
-              type="radio"
-              value="order"
-            />
-            <label for="orderRadioType">
+            <input id="orderRadioType" type="radio" />
+            <label>
               <span>Под пригон</span>
             </label>
           </div>
           <div className="item_column primary_column">
             <div className="select_transport">
-              <select id="categoriesss" className="e-form">
-                <option>Любой</option>
+              <select id="categories" className="e-form">
+                <option value="">dssd</option>
               </select>
               <div className="brand_choise">
                 <input
                   type="search"
                   id="brandTooltipBrandAutocompleteInput-brand"
                   placeholder="марка"
-                  value=""
                 />
-                <label
-                  for="brandTooltipBrandAutocompleteInput-brand"
-                  className="text"
-                ></label>
+                <label className="text"></label>
                 <span className="ac-clean">×</span>
               </div>
               <div className="model_choise">
                 <input
                   type="search"
                   id="brandTooltipBrandAutocompleteInput-model"
-                  placeholder="модель"
-                  value=""
                 />
-                <label
-                  for="brandTooltipBrandAutocompleteInput-model"
-                  data-text="Модель"
-                  className="text"
-                ></label>
+                <label data-text="Модель" className="text"></label>
                 <span className="ac-clean hide">×</span>
               </div>
             </div>
@@ -108,12 +86,7 @@ const searchFormTitle = () => {
           <div className="item_column secondary_column">
             <div className="m-rows">
               <div className="m-rows">
-                <label
-                  for="brandTooltipBrandAutocompleteInput-region"
-                  class="m-label"
-                >
-                  Регион
-                </label>
+                <label className="m-label">Регион</label>
               </div>
               <div
                 id="brandTooltipBrandAutocomplete-region"
@@ -124,19 +97,14 @@ const searchFormTitle = () => {
                   id="brandTooltipBrandAutocompleteInput-region"
                   placeholder="Регион"
                 />
-                <label
-                  for="brandTooltipBrandAutocompleteInput-region"
-                className="text"
-                ></label>
+                <label className="text"></label>
                 <span className="ac-clean hide">×</span>
-                <ul
-                  className="unstyle scrollbar autocomplete-select hide"
-                ></ul>
+                <ul className="unstyle scrollbar autocomplete-select hide"></ul>
               </div>
               <div className="m-rows e-year"></div>
             </div>
 
-            <label id="yearsLabel" for="yearFrom" className="m-label">
+            <label id="yearsLabel" className="m-label">
               Год
             </label>
             <div className="m-indent">
@@ -147,23 +115,16 @@ const searchFormTitle = () => {
                     placeholder="от"
                     className="e-form _grey"
                   >
-                    <option value="0" selected="selected">
-                      от
-                    </option>
+                    <option>от</option>
                   </select>
                 </div>
-                <select
-                  id="yearTo"
-                  className="e-form _grey"
-                >
-                  <option value="0" selected="selected">
-                    до
-                  </option>
+                <select id="yearTo" className="e-form _grey">
+                  <option>до</option>
                 </select>
               </div>
             </div>
             <div className="m-rows e-cost">
-              <label id="priceLabel" for="priceTo" class="m-label">
+              <label id="priceLabel" className="m-label">
                 Цена, $
               </label>
               <div className="m-indent">
