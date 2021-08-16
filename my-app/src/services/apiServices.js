cd496d2ce09ac83292e65ada60c8c1ef159312e4
ivan <your_email@whatever.com>
2021-08-16 21:41:52 +0200

    mm

---
+++ my-app/src/services/apiServices.js
@@ -0,0 +1,9 @@
+//import React from "react";
+
+  async function getTransportTypes(url) {
+    const response = await fetch(url);
+    const body = await response.json();
+    console.log(body)
+ return body
+  }
+   export default getTransportTypes(); 
